1180348c4bb285b2139a50a58016e29720e97539
zh123s <654707599@qq.com>
2021-11-07 23:08:37 +0800

    first day

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,6 +24,12 @@ Page({
       })
     }
   },
+  buttonClient:function(){
+    console.log("点击了按钮");
+    wx.navigateTo({
+      url: '../person/person',
+    })
+  },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
